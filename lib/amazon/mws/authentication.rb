1ff9795774dee193cc6fbb6655058642733d2492
Darcy Brown <darcy@darcybrown.com>
2018-08-02 20:17:54 -0500

    bump order version, cuz why not

--- lib/amazon/mws/authentication.rb
+++ lib/amazon/mws/authentication.rb
@@ -2,7 +2,7 @@ module Amazon
   module MWS
     class Authentication
       VERSION = '2009-01-01'
-      ORDERS_VERSION = '2011-01-01'
+      ORDERS_VERSION = '2013-09-01'
       INVENTORY_VERSION = '2010-10-01'
       #INVENTORY_VERSION = '2011-03-01'
     end
